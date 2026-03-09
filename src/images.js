f6f4463c482193fa750087178e57e8463df0a9ea
Magna <aceai2007@gmail.com>
2026-03-09 15:47:51 -0700

    cutormised some links

--- src/images.js
+++ src/images.js
@@ -1,4 +1,4 @@
-const images = [
+export const images = [
     // ANIME
     {
         src: "./images/anime/cap.jpg", id: 1,
@@ -17,51 +17,81 @@ const images = [
         category: "Anime", alt: "Sasuke cap", price: "2000",
         link:"https://wa.me/2348149657894?text=Welcome%20to%20Teesmart%20visual%20where%20you%20get%20anime%20mech%20customisation%20for%20cheap%20price.%20The%20cap%20you%20selected%20is%20%E2%82%A65000%20just%20send%20me%20a%20pic%20of%20the%20design%20you%20like",
     },{
-         src: "./images/anime/gojoframe.jpg", id: 1, category:"Anime",alt:"Gojo frame",price:"2000"
+        src: "./images/anime/gojoframe.jpg", id: 1, category: "Anime",
+        alt: "Gojo frame", price: "2000",
+        link: "https://wa.me/2348149657894?text=Welcome%20to%20Teesmart%20Visual%20what%20type%20of%20frame%20do%20you%20want"
     },{
-         src: "./images/anime/sungframe.jpg", id: 1, category:"Anime",alt:"Sung frame",price:"2000"
+        src: "./images/anime/sungframe.jpg", id: 1, category: "Anime",
+        alt: "Sung frame", price: "2000",
+        link: "https://wa.me/2348149657894?text=Welcome%20to%20Teesmart%20Visual%20what%20type%20of%20frame%20do%20you%20want"
     },{
-         src: "./images/anime/kanekiframe.jpg", id: 1, category:"Anime",alt:"Kaneki frame",price:"2000"
+        src: "./images/anime/kanekiframe.jpg", id: 1, category: "Anime",
+        alt: "Kaneki frame", price: "2000",
+        link: "https://wa.me/2348149657894?text=Welcome%20to%20Teesmart%20Visual%20what%20type%20of%20frame%20do%20you%20want"
     },{
-         src: "./images/anime/tanjiroframe.jpg", id: 1, category:"Anime",alt:"Tanjiro frame",price:"2000"
+        src: "./images/anime/tanjiroframe.jpg", id: 1, category: "Anime",
+        alt: "Tanjiro frame", price: "2000",
+        link: "https://wa.me/2348149657894?text=Welcome%20to%20Teesmart%20Visual%20what%20type%20of%20frame%20do%20you%20want"
     },
 
      // Native
      {
-         src: "./images/native/native.jpg", id: 1, category:"Native",alt:"Native cloth",price:"2000"
+         src: "./images/native/native.jpg", id: 1,
+         category: "Native", alt: "Native cloth", price: "2000",
+         link:"https://wa.me/234810524463?text=Welcome%20what%20will%20you%20like%20to%20order%20from%20us%20today"
     }, {
-         src: "./images/native/caps.jpg", id: 1, category:"Native",alt:"Native",price:"2000"
+         src: "./images/native/caps.jpg", id: 1,
+         category: "Native", alt: "Native", price: "2000",
+         link:"https://wa.me/234810524463?text=Welcome%20what%20will%20you%20like%20to%20order%20from%20us%20today"
      }, {
-         src: "./images/native/native2.jpg", id: 1, category:"Native",alt:"Native cloth",price:"2000"
+         src: "./images/native/native2.jpg", id: 1,
+         category: "Native", alt: "Native cloth", price: "2000",
+         link:"https://wa.me/234810524463?text=Welcome%20what%20will%20you%20like%20to%20order%20from%20us%20today"
     }, {
-         src: "./images/native/native6.jpg", id: 1, category:"Native",alt:"Native cloth",price:"2000"
+         src: "./images/native/native6.jpg", id: 1,
+         category: "Native", alt: "Native cloth", price: "2000",
+         link:"https://wa.me/234810524463?text=Welcome%20what%20will%20you%20like%20to%20order%20from%20us%20today"
      },
      // cosmetics
      {
-         src: "./images/lipgloss/lipbalm1.jpg", id: 1, category:"cosmetics",alt:"Lip-gloss",price:"2000"
+         src: "./images/lipgloss/lipbalm1.jpg", id: 1, category: "cosmetics",
+         alt: "Lip-gloss", price: "2000",
+         link:"https://wa.me/2349016867179?text=Welcome%20to%20Bisi%20Lip%20Care%20Package%20what%20will%20you%20like%20to%20order"
      }, {
-         src: "./images/lipgloss/lipbalmpack.jpg", id: 1, category:"cosmetics",alt:"Lip-balm",price:"2000"
+         src: "./images/lipgloss/lipbalmpack.jpg", id: 1,
+         category: "cosmetics", alt: "Lip-balm", price: "2000",
+         link:"https://wa.me/2349016867179?text=Welcome%20to%20Bisi%20Lip%20Care%20Package%20what%20will%20you%20like%20to%20order"
      }, {
-         src: "./images/lipgloss/lipglosspack.jpg", id: 1, category:"cosmetics",alt:"Lip-gloss",price:"2000"
+         src: "./images/lipgloss/lipglosspack.jpg", id: 1,
+         category: "cosmetics", alt: "Lip-gloss", price: "2000",
+         link:"https://wa.me/2349016867179?text=Welcome%20to%20Bisi%20Lip%20Care%20Package%20what%20will%20you%20like%20to%20order"
      }, {
-         src: "./images/lipgloss/lippark.jpg", id: 1, category:"cosmetics",alt:"Lip-gloss",price:"2000"
+         src: "./images/lipgloss/lippark.jpg", id: 1,
+         category: "cosmetics", alt: "Lip-gloss", price: "2000",
+         link:"https://wa.me/2349016867179?text=Welcome%20to%20Bisi%20Lip%20Care%20Package%20what%20will%20you%20like%20to%20order"
+
      },
      // snacks
      {
          src: "./images/snack/cake.jpg", id: 1,
-         category: "snack", alt: "Snacks", price: "2000"
+         category: "snack", alt: "Snacks", price: "2000",
+         link: "https://wa.me/2348064288340?text=Welcome%20to%20Kirahs%20Treat%20where%20your%20taste%20buds%20gets%20satisfied%20what%20will%20you%20like%20to%20get"
      },{
          src: "./images/snack/doughnut.jpg", id: 1,
-         category: "snack", alt: "Snacks", price: "2000"
+         category: "snack", alt: "Snacks", price: "2000",
+         link: "https://wa.me/2348064288340?text=Welcome%20to%20Kirahs%20Treat%20where%20your%20taste%20buds%20gets%20satisfied%20what%20will%20you%20like%20to%20get"
      },{
          src: "./images/snack/meatpie.jpg", id: 1,
-         category: "snack", alt: "Snacks", price: "2000"
+         category: "snack", alt: "Snacks", price: "2000",
+         link: "https://wa.me/2348064288340?text=Welcome%20to%20Kirahs%20Treat%20where%20your%20taste%20buds%20gets%20satisfied%20what%20will%20you%20like%20to%20get"
      },{
          src: "./images/snack/meatpie1.jpg", id: 1,
-         category: "snack", alt: "Snacks", price: "2000"
+         category: "snack", alt: "Snacks", price: "2000",
+         link: "https://wa.me/2348064288340?text=Welcome%20to%20Kirahs%20Treat%20where%20your%20taste%20buds%20gets%20satisfied%20what%20will%20you%20like%20to%20get"
      },{
          src: "./images/snack/milkydoughnut.jpg", id: 1,
-         category: "snack", alt: "Snacks", price: "2000"
+         category: "snack", alt: "Snacks", price: "2000",
+         link: "https://wa.me/2348064288340?text=Welcome%20to%20Kirahs%20Treat%20where%20your%20taste%20buds%20gets%20satisfied%20what%20will%20you%20like%20to%20get"
      },{
          src: "./images/snack/waffle.jpg", id: 1,
          category: "snack", alt: "Snacks", price: "2000"
@@ -71,10 +101,10 @@ const images = [
      },{
          src: "./images/snack/puff.jpg", id: 1,
          category: "snack", alt: "Snacks", price: "2000",
-         link: "https://wa.me/2348140182558%3Ftext%3Dhello%2520i%2520am%2520nobody%2520lets%2520chat"
+         link: "https://wa.me/2348064288340?text=Welcome%20to%20Kirahs%20Treat%20where%20your%20taste%20buds%20gets%20satisfied%20what%20will%20you%20like%20to%20get"
      },
 
 
 ]
     
-    export {images}
+    
